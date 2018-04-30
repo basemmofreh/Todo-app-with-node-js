4672d31e392a8ef0eceaedbbe951cf76131affa6
Bassem <AnError@live.com>
2018-04-29 20:48:48 -0800

    add tests for /POST/users/login

--- tests/server.test.js
+++ tests/server.test.js
@@ -4,7 +4,7 @@ const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const Todo = require('./../models/todo');
-
+const User = require('./../models/user');
 const {todos,populateTodos,users,populateUsers} = require('./seed/seed');
 // debugger;
 beforeEach(populateUsers);
@@ -192,7 +192,7 @@ describe('POST /users',()=>{
         .send({email,password})
         .expect(400)
         .expect((res)=>{
-          console.log(res.body.errors);
+          console.log(res.body.errors.email.message);
         })
         .end(done);
   })
@@ -210,4 +210,35 @@ describe('POST /users',()=>{
         .end(done);
   })
 
+})
+describe('POST /users/login',()=>{
+    it('should login user and return auth token',(done)=>{
+        request(app)
+          .post('/users/login')
+          .send({email:users[0].email,password:users[0].password})
+          .expect(200)
+          .expect((res)=>{
+              expect(res.headers['x-auth']).toExist();
+          })
+          .end((err,res)=>{
+            if(err)
+              return done(err);
+
+            User.findById(users[0]._id).then((user)=>{
+                expect(user.tokens[1]).toInclude({
+                  access:'auth',
+                  token: res.headers['x-auth']
+                });
+                done();
+            }).catch((e)=>done(e));
+          })
+    })
+
+    it('should reject invalid login',(done)=>{
+        request(app)
+          .post('/users/login')
+          .send({email:users[0].email+'a',password:users[0].password+'1'})
+          .expect(400)
+          .end(done);
+    })
 })
